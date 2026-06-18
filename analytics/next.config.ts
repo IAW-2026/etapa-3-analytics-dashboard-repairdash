d2031db3d61109d51fe0e40411140eca15b21c5e
FranRK <franciscorecaldee@gmail.com>
2026-06-18 18:07:47 -0300

    arreglo next.config

--- analytics/next.config.ts
+++ analytics/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    browsersListForSwc: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
